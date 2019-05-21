4f11f6baad39248e650c4ae37a439f77a5a93b66
Austin Gehlhaus <50186604+Gehlhaus@users.noreply.github.com>
2019-05-20 20:34:46 -0400

    Updated script.js

--- project/js/script.js
+++ project/js/script.js
@@ -15,13 +15,43 @@ function callacc() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
             document.getElementById("accountheadings").innerHTML = this.responseText;
         }
     };
     xmlhttp.open("GET", "../php/accountinfo.php?q=", true);
     xmlhttp.send(); 
+
+    getTransactions();
 }
 
+function getTransactions(){
+    var xmlhttp2 = new XMLHttpRequest();
+    xmlhttp2.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var table = document.getElementById("transTable");
+            var transArray = this.responseText.split("^");
+            transArray.pop();
+            transArray.forEach(function(transaction){
+                var transDetail = transaction.split("*");
+                var row = createRow(transDetail);
+                table.appendChild(row);
+            });
+        }
+    };
+    xmlhttp2.open("GET", "../php/transactionHistory.php", true);
+    xmlhttp2.send(); 
+}
+
+function createRow(detail){
+    var row = document.createElement("TR");
+    var counter = 0;
+    detail.forEach(function(cell){
+        var x = row.insertCell(counter++);
+        x.innerHTML = cell;
+    });
+    return row;
+}
 
 
 
