d186b91680a2706c822abcccd46bc96065639b55
arif-banai <banai.arif@gmail.com>
2019-05-20 23:05:26 -0400

    Implemented buying coins in buy.php

--- project/php/buy.php
+++ project/php/buy.php
@@ -1,45 +1,61 @@
 <?php
 	session_start();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<?php
 	if($_SESSION["user"] == ""){
-		echo '<h1>You must be logged in to buy QCoins</h1><br><a href="../html/login.html">LogIn</a>';
-	}
-	else{
-		$servername = "mars.cs.qc.cuny.edu";
-		$username = "kiye0230"; // your venus login
-		$password = "23550230"; // your venus password
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password);
-
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		} else {
+        echo '<h1>You must be logged in. </h1><br><a href="../html/login.html">Log In</a>';
 		
-			$db_selected = mysqli_select_db($conn, 'kiye0230');// your venus login
-			if (!$db_selected) {
-		    	die ('Can\'t use DB : ' . mysql_error());
-			} else{
-				$sql = "SELECT coins FROM accounts WHERE username = '$user'";
-				$result = $conn->query($sql);
-				echo $result;
-				if ($result->num_rows > 0) {
-				    echo '<h1>', $_SESSION["user"], '\'s balance is: ', $result, ' QCoins</h1>';
-				} else {
-			    	echo "Error";
+    } else {
+		include 'functions.php';
+		$amt = $_POST["amountToBuy"];
+		
+        $user = $_SESSION["user"];
+		
+        $servername = "mars.cs.qc.cuny.edu";
+        $username = "kiye0230";
+        $password = "23550230";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+    
+            $db_selected = mysqli_select_db($conn, 'kiye0230');
+            if (!$db_selected) {
+                die ('Can\'t use DB : ' . mysql_error());
+            } else{
+                //here good
+                 
+                $sql = "SELECT coins FROM accounts WHERE username = '$user';";
+                $result =  $conn->query($sql);
+				
+				if(!$result) {
+					echo 'Could not run query: ' . mysql_error();
+					exit;
 				}
-			}
-		}
-	}
-	$conn->close();	
+				
+				$row=$result->fetch_assoc();
+                $userbank = (int) $row['coins'];
+				
+				$total = $userbank + $amt;
+				
+				$sql = "UPDATE accounts SET coins = $total WHERE username = '$user';";
+                $conn->query($sql);
+				
+				$sql = "INSERT INTO transactions(id, sender, receiver, amount) VALUES (DEFAULT, 'QCOIN', '$user', $amt);";
+                $conn->query($sql);
+				
+				echo '<p>Transaction completed.</p><a href="../html/buy.html">Click to return to the Buy page.</a>';
+                
+             
+          
+				//here good                    
+            }
+        }
+    }    
+        
+
+
+
 ?>
-</body>
-</html>
