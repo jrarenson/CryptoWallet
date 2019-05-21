01cb846444189962a0b15dc0c0b77fa15e116e91
arif-banai <banai.arif@gmail.com>
2019-05-21 02:24:57 -0400

    Minor edit to session.php to link to buysell.html and remove crossout line

--- project/php/session.php
+++ project/php/session.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
       if($_SESSION["loggedin"] == 1){
-        echo '<a class="btn btn-primary notComplete" href="../html/buy.html"><h3>Buy/Sell</h3></a>';
+        echo '<a class="btn btn-primary" href="../html/buysell.html"><h3>Buy/Sell</h3></a>';
         echo '<a class="btn btn-primary" href="../html/send.html"><h3>Send</h3></a>';
         echo '<a class="btn btn-primary" href="../php/graphs.php"><h3>Graphs</h3></a>'; 
         echo '<a class="btn btn-primary" href="../html/myAccount.html"><h3>My Account</h3></a>';
