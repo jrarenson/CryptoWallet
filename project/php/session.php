c78078435bdd9d918fcc67cead2d4701bba45163
Austin Gehlhaus <50186604+Gehlhaus@users.noreply.github.com>
2019-05-20 20:57:46 -0400

    Add files via upload

--- project/php/session.php
+++ project/php/session.php
@@ -1,8 +1,8 @@
 <?php 
 session_start();
       if($_SESSION["loggedin"] == 1){
-        echo '<a class="btn btn-primary notComplete" href="../php/buy.php"><h3>Buy/Sell</h3></a>';
-        echo '<a class="btn btn-primary" href="../php/send.php"><h3>Send</h3></a>';
+        echo '<a class="btn btn-primary notComplete" href="../html/buy.html"><h3>Buy/Sell</h3></a>';
+        echo '<a class="btn btn-primary" href="../html/send.html"><h3>Send</h3></a>';
         echo '<a class="btn btn-primary" href="../php/graphs.php"><h3>Graphs</h3></a>'; 
         echo '<a class="btn btn-primary" href="../html/myAccount.html"><h3>My Account</h3></a>';
         echo '<a class="btn btn-primary" href="../php/logout.php"><h3>Sign Out</h3></a>';
