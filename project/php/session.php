0b8d44025076a5849fa451054fc0fd1b5973e927
Austin Gehlhaus <50186604+Gehlhaus@users.noreply.github.com>
2019-05-20 20:47:22 -0400

    Updated session.php (removing line from buttons)

--- project/php/session.php
+++ project/php/session.php
@@ -1,10 +1,9 @@
 <?php 
 session_start();
       if($_SESSION["loggedin"] == 1){
-        echo '<a class="btn btn-primary notComplete" href="../php/buy.php"><h3>Buy</h3></a>';
-        echo '<a class="btn btn-primary notComplete" href="../php/sell.php"><h3>Sell</h3></a>';
-        echo '<a class="btn btn-primary notComplete" href="../php/send.php"><h3>Send</h3></a>';
-        echo '<a class="btn btn-primary notComplete" href="../php/graphs.php"><h3>Graphs</h3></a>'; 
+        echo '<a class="btn btn-primary notComplete" href="../php/buy.php"><h3>Buy/Sell</h3></a>';
+        echo '<a class="btn btn-primary" href="../php/send.php"><h3>Send</h3></a>';
+        echo '<a class="btn btn-primary" href="../php/graphs.php"><h3>Graphs</h3></a>'; 
         echo '<a class="btn btn-primary" href="../html/myAccount.html"><h3>My Account</h3></a>';
         echo '<a class="btn btn-primary" href="../php/logout.php"><h3>Sign Out</h3></a>';
         echo 'Logged in as: ', $_SESSION["user"];
