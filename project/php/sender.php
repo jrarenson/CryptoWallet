12187b74f904e8a07b60850ea7cc400c23f2782d
Austin Gehlhaus <50186604+Gehlhaus@users.noreply.github.com>
2019-05-20 22:51:49 -0400

    Add files via upload

--- project/php/sender.php
+++ project/php/sender.php
@@ -1,10 +1,14 @@
 <?php
 	session_start();
-
+	if($_SESSION["user"] == ""){
+        echo '<h1>You must be logged in. </h1><br><a href="../html/login.html">LogIn</a>';
+    }
+    else{
     include 'functions.php';
     $recp = $_POST["recipient"];
     $amt = $_POST["amountToSend"];
 
+    
         $user = $_SESSION["user"];
         $servername = "mars.cs.qc.cuny.edu";
         $username = "kiye0230";
@@ -26,46 +30,60 @@
                  
                 $sql = "SELECT username FROM accounts WHERE username = '$recp'";
                 $result = $conn->query($sql);
-                //check
+                
                 if ($result->num_rows > 0) {
                     $sql = "SELECT coins FROM accounts WHERE username = '$user'";
                     $result =  $conn->query($sql);
 					
-					if(!$result) { 
+					if(!$result) {
 						echo 'Could not run query: ' . mysql_error();
 						exit;
 					}
+                    $row=$result->fetch_assoc();
+                    $sendbank = (int) $row['coins'];
+                  
+                  
+                    $total = $sendbank - $amt;
+                    if ($total >= 0) { 
+                    
+                    $sql = "UPDATE accounts SET coins =  $total WHERE username = '$user' ";
+                    $conn->query($sql);
+
+                    
+                    $sql = "SELECT coins FROM accounts WHERE username = '$recp'";
+                    $result =  $conn->query($sql);
 					
-					$row = $result->fetch_assoc();
-					$sendbank = (int) $row['coins'];
-					
-                    if ($amt > $sendbank) {
-                        echo 'Too much man u cant do that';
-                    } else {
-                        $sql = "UPDATE accounts SET coins = " . $sendbank - $amt . " WHERE username = '$user'";
-                        $conn->query($sql);
-						
-                        $sql = "SELECT coins FROM accounts WHERE username = '$recp'";
-                        $result = $conn->query($sql);
-						
-						if(!$result) {
-							echo 'Could not run query: ' . mysql_error();
-							exit;
-						}
-						
-						$row = $result->fetch_assoc();
-						$recpbank = (int) $row['coins'];
-						
-                        $sql = "UPDATE accounts SET coins = " . $recpbank + $amt . " WHERE username = '$recp'";
-                        $conn->query($sql);
-                    }                          
+					if(!$result) {
+						echo 'Could not run query: ' . mysql_error();
+						exit;
+					}
+                    $row=$result->fetch_assoc();
+                    $recpbank = (int) $row['coins'];
+
+                    $total = $recpbank + $amt;
+                    $sql = "UPDATE accounts SET coins =  $total WHERE username = '$recp' ";
+                    $conn->query($sql);
+       
+                    $sql = " INSERT INTO transactions(id, sender, receiver, amount) VALUES (DEFAULT, '$user', '$recp', $amt)";
+                    $conn->query($sql);
+                    echo '<p>Transaction completed.</p><a href="../html/send.html">Click to return to Send page.</a>';
+
+                                       
+                } else {
+                    echo '<p>You do not have enough coins to send.</p><a href="../html/send.html">Click to return to Send page.</a>';
                 }
-                echo 'Transaction completed';
-            //here good                    
+            } 
+             
+            $conn->close();   
+            header("Location: ../html/send.html");                  
             }
         }
-        $conn->close();
+    }    
+        
 
 
 
 ?>
+
+
+
