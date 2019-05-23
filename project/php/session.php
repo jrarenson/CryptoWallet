cd88a2f0e55752a5e01abaa6d8396c6e4f7b4cb1
jrarenson <40274218+jrarenson@users.noreply.github.com>
2019-05-23 11:02:37 -0400

    changed graphs to html

--- project/php/session.php
+++ project/php/session.php
@@ -4,7 +4,7 @@ session_start();
 	echo '<a class="btn btn-primary" href="../html/home.html"><h3>Home</h3></a>';
         echo '<a class="btn btn-primary" href="../html/buysell.html"><h3>Buy/Sell</h3></a>';
         echo '<a class="btn btn-primary" href="../html/send.html"><h3>Send</h3></a>';
-        echo '<a class="btn btn-primary" href="../php/graphs.php"><h3>Graphs</h3></a>'; 
+        echo '<a class="btn btn-primary" href="../html/graphs.html"><h3>Graphs</h3></a>'; 
         echo '<a class="btn btn-primary" href="../html/plans.html"><h3>Plans</h3></a>'; 
         echo '<a class="btn btn-primary" href="../html/myAccount.html"><h3>My Account</h3></a>';
         echo '<a class="btn btn-primary" href="../php/logout.php"><h3>Sign Out</h3></a>';
